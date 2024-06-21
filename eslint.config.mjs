2e4e6b55422164003b08d889f9fab6e6f47fc98c
Shimpei Iwamaru <gangan.education@gmail.com>
2024-06-22 02:46:31 +0900

    chore: setup ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,19 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt({
-  // Your custom configs here
+  ignores: ['.github/**/*', '.vscode/**']
 })
+  .override('nuxt/typescript/rules', {
+    rules: {
+      '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+      // TODO: 一時的にOFF
+      '@typescript-eslint/explicit-function-return-type': 'off'
+    }
+  })
+  .override('nuxt/vue/rules', {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/no-multiple-template-root': 'off'
+    }
+  });
